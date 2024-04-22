92d4e19ec396990dd5c9215c1cd734647eb26b45
bluebird <linlanxi7552659@gmail.com>
2024-04-22 14:02:34 +0800

    #feat crt FusionRecipes.java support

---
+++ src/main/java/com/hbm/handler/crt/Fusion.java
@@ -0,0 +1,84 @@
+package com.hbm.handler.crt;
+
+
+import com.hbm.inventory.FusionRecipes.FusionRecipeData;
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.liquid.ILiquidDefinition;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import static com.hbm.inventory.FusionRecipes.RECIPE_DATA_MAP;
+
+@ZenRegister
+@ZenClass("mods.ntm.Fusion")
+public class Fusion {
+
+
+	// 实现为 private static final LinkedHashMap<Fluid, FusionRecipeData> RECIPE_DATA_MAP = new LinkedHashMap<>(); 增加和删除配方
+	private static class ActionAddRecipe implements IAction {
+		private final int byproductChance;
+		private final ItemStack byproduct;
+		private final int breedingLevel;
+		private final int steamProduction;
+
+		Fluid input;
+
+		public ActionAddRecipe(Fluid input, IItemStack byproduct, int byproductChance, int breedingLevel, int steamProduction) {
+
+			this.input = input;
+			this.byproduct = CraftTweakerMC.getItemStack(byproduct);
+			this.byproductChance = byproductChance;
+			this.breedingLevel = breedingLevel;
+			this.steamProduction = steamProduction;
+		}
+
+		@Override
+		public void apply() {
+			RECIPE_DATA_MAP.put(this.input, new FusionRecipeData(this.byproductChance, this.byproduct, this.breedingLevel, this.steamProduction));
+		}
+
+		@Override
+		public String describe() {
+
+			return  "add Fusion Recipe for input "+this.input+" with byproduct "+this.byproduct+" with byproductChance "+this.byproductChance+" with breedingLevel "+this.breedingLevel+" with steamProduction "+this.steamProduction;
+
+		}
+	}
+	@ZenMethod
+	public static void addFusionRecipe(ILiquidDefinition input, IItemStack output, int chance, int breedingLevel, int steamProduction) {
+		CraftTweakerAPI.apply(new ActionAddRecipe(CraftTweakerMC.getFluid(input), output, chance, breedingLevel, steamProduction));
+	}
+
+
+	@ZenMethod
+	public static void removeFusionRecipe(ILiquidDefinition input) {
+		CraftTweakerAPI.apply(new ActionRemoveRecipe(CraftTweakerMC.getFluid(input)));
+	}
+
+	private static class ActionRemoveRecipe implements IAction{
+
+		Fluid input;
+		public ActionRemoveRecipe(Fluid fluid) {
+
+			this.input = fluid;
+		}
+
+		@Override
+		public void apply() {
+
+			RECIPE_DATA_MAP.remove(this.input);
+		}
+
+		@Override
+		public String describe() {
+
+			return "Removing NTM Fusion recipe for input "+this.input;
+		}
+	}
+}
--- src/main/java/com/hbm/inventory/FusionRecipes.java
+++ src/main/java/com/hbm/inventory/FusionRecipes.java
@@ -2,78 +2,67 @@ package com.hbm.inventory;
 
 import com.hbm.forgefluid.ModForgeFluids;
 import com.hbm.items.ModItems;
-
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.Fluid;
 
+import java.util.LinkedHashMap;
+
 public class FusionRecipes {
 
+	public static final LinkedHashMap<Fluid, FusionRecipeData> RECIPE_DATA_MAP = new LinkedHashMap<>();
+
+	static {
+		RECIPE_DATA_MAP.put(ModForgeFluids.plasma_dt, new FusionRecipeData(1200, new ItemStack(ModItems.pellet_charged), 2, 225));
+		RECIPE_DATA_MAP.put(ModForgeFluids.plasma_hd, new FusionRecipeData(1200, new ItemStack(ModItems.pellet_charged), 1, 150));
+		RECIPE_DATA_MAP.put(ModForgeFluids.plasma_ht, new FusionRecipeData(1200, new ItemStack(ModItems.pellet_charged), 1, 188));
+		RECIPE_DATA_MAP.put(ModForgeFluids.plasma_xm, new FusionRecipeData(2400, new ItemStack(ModItems.powder_chlorophyte), 3, 450));
+		RECIPE_DATA_MAP.put(ModForgeFluids.plasma_put, new FusionRecipeData(2400, new ItemStack(ModItems.powder_xe135), 4, 600));
+		RECIPE_DATA_MAP.put(ModForgeFluids.plasma_bf, new FusionRecipeData(150, new ItemStack(ModItems.powder_balefire), 5, 1200));
+	}
+
 	public static int getByproductChance(Fluid plasma) {
-		if(plasma == ModForgeFluids.plasma_dt){
-			return 1200; 
-		} else if(plasma == ModForgeFluids.plasma_hd){
-			return 1200;
-		} else if(plasma == ModForgeFluids.plasma_ht){
-			return 1200;
-		} else if(plasma == ModForgeFluids.plasma_xm){
-			return 2400;
-		} else if(plasma == ModForgeFluids.plasma_put){
-			return 2400;
-		} else if(plasma == ModForgeFluids.plasma_bf){
-			return 150;
-		}
-		return 0;
+		return RECIPE_DATA_MAP.getOrDefault(plasma, new FusionRecipeData(0, ItemStack.EMPTY, 0, 0)).getByproductChance();
 	}
 
 	public static ItemStack getByproduct(Fluid plasma) {
-		if(plasma == ModForgeFluids.plasma_dt){
-			return new ItemStack(ModItems.pellet_charged); 
-		} else if(plasma == ModForgeFluids.plasma_hd){
-			return new ItemStack(ModItems.pellet_charged);
-		} else if(plasma == ModForgeFluids.plasma_ht){
-			return new ItemStack(ModItems.pellet_charged);
-		} else if(plasma == ModForgeFluids.plasma_xm){
-			return new ItemStack(ModItems.powder_chlorophyte);
-		} else if(plasma == ModForgeFluids.plasma_put){
-			return new ItemStack(ModItems.powder_xe135);
-		} else if(plasma == ModForgeFluids.plasma_bf){
-			return new ItemStack(ModItems.powder_balefire);
-		}
-		return ItemStack.EMPTY;
-	}
-	
-	public static int getBreedingLevel(Fluid plasma) {
-		if(plasma == ModForgeFluids.plasma_dt){
-			return 2;
-		} else if(plasma == ModForgeFluids.plasma_hd){
-			return 1;
-		} else if(plasma == ModForgeFluids.plasma_ht){
-			return 1;
-		} else if(plasma == ModForgeFluids.plasma_xm){
-			return 3;
-		} else if(plasma == ModForgeFluids.plasma_put){
-			return 4;
-		} else if(plasma == ModForgeFluids.plasma_bf){
-			return 5;
-		}
-		return 0;
-	}
-	
-	public static int getSteamProduction(Fluid plasma) {
-		if(plasma == ModForgeFluids.plasma_dt){
-			return 225;
-		} else if(plasma == ModForgeFluids.plasma_hd){
-			return 150;
-		} else if(plasma == ModForgeFluids.plasma_ht){
-			return 188;
-		} else if(plasma == ModForgeFluids.plasma_xm){
-			return 450;
-		} else if(plasma == ModForgeFluids.plasma_put){
-			return 600;
-		} else if(plasma == ModForgeFluids.plasma_bf){
-			return 1200;
-		}
-		return 0;
+		return RECIPE_DATA_MAP.getOrDefault(plasma, new FusionRecipeData(0, ItemStack.EMPTY, 0, 0)).getByproduct();
 	}
 
+	public static int getBreedingLevel(Fluid plasma) {
+		return RECIPE_DATA_MAP.getOrDefault(plasma, new FusionRecipeData(0, ItemStack.EMPTY, 0, 0)).getBreedingLevel();
+	}
+
+	public static int getSteamProduction(Fluid plasma) {
+		return RECIPE_DATA_MAP.getOrDefault(plasma, new FusionRecipeData(0, ItemStack.EMPTY, 0, 0)).getSteamProduction();
+	}
+
+	public static class FusionRecipeData {
+		private final int byproductChance;
+		private final ItemStack byproduct;
+		private final int breedingLevel;
+		private final int steamProduction;
+
+		public FusionRecipeData(int byproductChance, ItemStack byproduct, int breedingLevel, int steamProduction) {
+			this.byproductChance = byproductChance;
+			this.byproduct = byproduct;
+			this.breedingLevel = breedingLevel;
+			this.steamProduction = steamProduction;
+		}
+
+		public int getByproductChance() {
+			return byproductChance;
+		}
+
+		public ItemStack getByproduct() {
+			return byproduct;
+		}
+
+		public int getBreedingLevel() {
+			return breedingLevel;
+		}
+
+		public int getSteamProduction() {
+			return steamProduction;
+		}
+	}
 }
